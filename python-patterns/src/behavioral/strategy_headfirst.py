1cd2b4311c6916cb3c151c84985590cbe940d113
pq <pq.blogspot@gmail.com>
2017-05-01 21:40:15 +0900

    strategy_headfirst.py

--- python-patterns/src/behavioral/strategy_headfirst.py
+++ python-patterns/src/behavioral/strategy_headfirst.py
@@ -1,81 +1,64 @@
 #!~/anaconda3/bin/python
 # -*- coding: utf-8 -*-
-import types
-
-class Duck:
-    def __init__(self):
-        self.flyBehavior
-        self.quackBehavior
-
-        
-        
-
-    def setFlyBehavior(self,func=None):
-        if func is not None:
-            self.performFly = types.MethodType(func, self)        
-    def setQuackBehavior(self,func=None):
-        if func is not None:
-            self.performQuack = types.MethodType(func, self)    
-    def display(self):
-        pass
-    def performFly(self):
-        pass
-    def performQuack(self):
-        pass
+def flyNoWay():
+    print("I can't fly")
+def flyWithWings():
+    print("I'm flying!!")    
+def flyRocketPowered():
+    print("I'm flying with a rocket")       
+def muteQuack():
+    print("<< Silence >>")
+def quack():
+    print("Quack")
+def squeak():
+    print("Squeak")
+def fakeQuack():
+    print("Qwak")
+class Duck:    
     def swim(self):
-        print("All ducks float, even decoys!")
-class FlayBehavior:
-    def __init__(self,func=None):
-        if func is not None:
-            self.fly = types.MethodType(func, self)
-    def fly(self):
-        pass     
-class FlyWithWings:
-    def fly(self):
-        print("I'm flying!!")        
-class FlyRocketPowered:
-    def fly(self):
-        print("I'm flying with a rocket")   
-class FlyNoWay:
-    def fly(self):
-        print("I can't fly")           
-
-
+        print("All ducks float, even decoys!") 
 class DecoyDuck(Duck):
     def __init__(self):
-        self.flyBehavior = None 
-        self.quackBehavior = None
+        self.performFly = flyNoWay 
+        self.performQuack = muteQuack
     def display(self):
         print("I'm a duck Decoy")
 class MallardDuck(Duck):
     def __init__(self):
-        self.flyBehavior = None 
-        self.quackBehavior = None
+        self.performFly = flyWithWings
+        self.performQuack = quack
     def display(self):
         print("I'm a real Mallard duck")
 class ModelDuck(Duck):
     def __init__(self):
-        self.flyBehavior = None 
-        self.quackBehavior = None
+        self.performFly = flyNoWay 
+        self.performQuack = quack
     def display(self):
         print("I'm a model duck")
-class RubberDuck(Duck):
-    def __init__(self):
-        self.flyBehavior = None 
-        self.quackBehavior = None
-    def display(self):
-        print("I'm a rubber duckie")
 class RedHeadDuck(Duck):
     def __init__(self):
-        self.flyBehavior = None 
-        self.quackBehavior = None
+        self.performFly = flyWithWings
+        self.performQuack = quack
     def display(self):
-        print("I'm a real Red Headed duck")
-        
-        
-        
-        
-        
-        
+        print("I'm a real Red Headed duck")     
+class RubberDuck(Duck):
+    def __init__(self):
+        self.performFly = flyNoWay 
+        self.performQuack = squeak
+    def display(self):
+        print("I'm a rubber duckie")   
 if __name__ == '__main__':
-    pass
+    mallard = MallardDuck()
+    rubberDuckie = RubberDuck()
+    decoy = DecoyDuck()
+    model = ModelDuck()
+    
+    mallard.performQuack()
+    rubberDuckie.performQuack()
+    decoy.performQuack()
+   
+    model.performFly()  
+    model.performFly = flyRocketPowered
+    model.performFly()
+    
+    
