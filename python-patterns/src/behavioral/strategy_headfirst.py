924a5f9a693d71c93fa7c6373d2023874bccefa7
pq <pq.blogspot@gmail.com>
2017-05-04 14:47:53 +0900

    aggregation

--- python-patterns/src/behavioral/strategy_headfirst.py
+++ python-patterns/src/behavioral/strategy_headfirst.py
@@ -1,8 +1,6 @@
 #!~/anaconda3/bin/python
 # -*- coding: utf-8 -*-
 class FlyBehavior:
-#     def __init__(self,name):
-#         self.name = name
     def flyNoWay(self):
         print(self.name + " can't fly")
     def flyWithWings(self):
@@ -28,54 +26,59 @@ class Duck:
 class DecoyDuck(Duck):
     def __init__(self,name,fb):
         self.name = name
-        self.flyBehavior = FlyBehavior(name)
-        self.performFly = self.flyBehavior.flyWithWings
+        fb.name = name
+        self.performFly = fb.flyWithWings
         self.performQuack = QuackBehavior.quack        
     def display(self):
         print(self.name + " is a duck Decoy")
 class MallardDuck(Duck):
-    def __init__(self,name):
-        self.flyBehavior = FlyBehavior(name)
-        self.performFly = self.flyBehavior.flyWithWings
+    def __init__(self,name,fb):
+        self.name = name
+        fb.name = name
+        self.performFly = fb.flyWithWings
         self.performQuack = QuackBehavior.quack
     def display(self):
         print(self.name + " is a real Mallard duck")
 class ModelDuck(Duck):
-    def __init__(self,name):
-        self.flyBehavior = FlyBehavior(name)
-        self.performFly = self.flyBehavior.flyNoWay 
+    def __init__(self,name,fb):
+        self.name = name
+        fb.name = name
+        self.performFly = fb.flyNoWay 
         self.performQuack = QuackBehavior.quack
     def display(self):
         print(self.name + " is a model duck")
 class RedHeadDuck(Duck):
-    def __init__(self,name):
-        self.flyBehavior = FlyBehavior(name)
-        self.performFly = self.flyBehavior.flyWithWings
+    def __init__(self,name,fb):
+        self.name = name
+        fb.name = name
+        self.performFly = fb.flyWithWings
         self.performQuack = QuackBehavior.quack
     def display(self):
         print(self.name + " is a real Red Headed duck")     
 class RubberDuck(Duck):
-    def __init__(self,name):
-        self.flyBehavior = FlyBehavior(name)
-        self.performFly = self.flyBehavior.flyNoWay 
+    def __init__(self,name,fb):
+        self.name = name
+        fb.name = name
+        self.performFly = fb.flyNoWay 
         self.performQuack = QuackBehavior.squeak
     def display(self):
         print(self.name + " is a rubber duckie")   
 if __name__ == '__main__':
-    mallard = MallardDuck("mallard")
-    rubberDuckie = RubberDuck("rubberDukie")
-    decoy = DecoyDuck("decoy")
-    model = ModelDuck("model")
+    fb = FlyBehavior()
+    mallard = MallardDuck("mallard",fb)
+    rubberDuckie = RubberDuck("rubberDukie",fb)
+    decoy = DecoyDuck("decoy",fb)
+    model = ModelDuck("model",fb)
     
     mallard.performQuack()
     rubberDuckie.performQuack()
     decoy.performQuack()
    
     model.performFly()  
-    model.performFly = model.flyBehavior.flyRocketPowered  # インスタンスのメソッドを置換。
+    model.performFly = fb.flyRocketPowered  # インスタンスのメソッドを置換。
     model.performFly()
     
-    model2 = ModelDuck("model2")
+    model2 = ModelDuck("model2",fb)
     model2.performFly()  # 他のインスタンスのメソッドは置換されない。
     
 
