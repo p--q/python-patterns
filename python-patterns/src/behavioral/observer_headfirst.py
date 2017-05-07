063d42c38b2c909e1ad64915001c4d1992b413ab
pq <pq.blogspot@gmail.com>
2017-05-07 15:09:06 +0900

    Observerのみの実装。remove不可。不要コード削除。

--- python-patterns/src/behavioral/observer_headfirst.py
+++ python-patterns/src/behavioral/observer_headfirst.py
@@ -16,14 +16,9 @@ class WeatherData:  # Subject
     def getPressure(self):
         return self.pressure
 class CurrentConditionsDisplay:  # Observer
-#     def __init__(self,weatherData):
     def observe(self,weatherData):
         weatherData.measurementsChanged = types.MethodType(self.register(weatherData.measurementsChanged), weatherData)
-#     def observe(self,bound_method):
-#         bound_method.__self__.measurementsChanged = types.MethodType(self.register(bound_method), bound_method.__self__)   
     def register(self,f):
-        
-        
         def g(this):
             f()
             return self.update(this)
@@ -34,15 +29,11 @@ class CurrentConditionsDisplay:  # Observer
         self.display()
     def display(self):
         print("Current conditions: {}F degrees and {}% humidity".format(self.temperature, self.humidity))
-class ForecastDisplay:
+class ForecastDisplay:  # Observer
     def __init__(self):
-#     def __init__(self,weatherData):
-#         weatherData.measurementsChanged = types.MethodType(self.register(weatherData.measurementsChanged), weatherData)
         self.currentPressure = 29.92
     def observe(self,weatherData):
         weatherData.measurementsChanged = types.MethodType(self.register(weatherData.measurementsChanged), weatherData)
-#     def observe(self,bound_method):
-#         bound_method = types.MethodType(self.register(bound_method), bound_method.__self__)    
     def register(self,f):
         def g(this):
             f()
@@ -60,10 +51,8 @@ class ForecastDisplay:
             print("More of the same");
         elif self.currentPressure < self.lastPressure:
             print("Watch out for cooler, rainy weather")
-class StatisticsDisplay:
+class StatisticsDisplay:  # Observer
     def __init__(self):
-#     def __init__(self,weatherData):
-#         weatherData.measurementsChanged = types.MethodType(self.register(weatherData.measurementsChanged), weatherData)
         self.maxTemp = 0.0
         self.minTemp = 200
         self.tempSum = 0.0
@@ -85,10 +74,8 @@ class StatisticsDisplay:
         self.display()
     def display(self):
         print("Avg/Max/Min temperature = {}/{}/{}".format(self.tempSum/self.numReadings, self.maxTemp, self.minTemp))
-class HeatIndexDisplay:
+class HeatIndexDisplay:  # Observer
     def __init__(self):
-#     def __init__(self,weatherData):
-#         weatherData.measurementsChanged = types.MethodType(self.register(weatherData.measurementsChanged), weatherData)
         self.heatIndex = 0.0
     def observe(self,weatherData):
         weatherData.measurementsChanged = types.MethodType(self.register(weatherData.measurementsChanged), weatherData)
@@ -104,50 +91,19 @@ class HeatIndexDisplay:
         return (16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) + (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) + (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) + (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 * (rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) + (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) + 0.000000000843296 * (t * t * rh * rh * rh)) - (0.0000000000481975 * (t * t * t * rh * rh * rh))
     def display(self):
         print("Heat index is {}".format(self.heatIndex))      
-
-
 if __name__ == '__main__':
-    
-#     def register(self):
-#         callbacks = list()
-#         def g(this):
-#             callbacks.append(self.update(this))
-#             return lambda callbacks: [fn() for fn in callbacks]
-#         return g    
-
-
-
-
-
-    weatherData = WeatherData()
-#     currentDisplay = CurrentConditionsDisplay(weatherData)
-#     statisticsDisplay = StatisticsDisplay(weatherData)
-#     forecastDisplay = ForecastDisplay(weatherData)
-#     heatIndexDisplay = HeatIndexDisplay(weatherData)
-
-
-    currentDisplay = CurrentConditionsDisplay()
-    statisticsDisplay = StatisticsDisplay()
-    forecastDisplay = ForecastDisplay()
-    heatIndexDisplay = HeatIndexDisplay()
-    
-    
-    currentDisplay.observe(weatherData)
-    statisticsDisplay.observe(weatherData)
-    forecastDisplay.observe(weatherData)
-    heatIndexDisplay.observe(weatherData)
-    
-#     weatherData = WeatherData()
-#     currentDisplay.observe(weatherData)
-    
-    
-#     forecastDisplay.update = types.MethodType(lambda :pass, forecastDisplay)
-    
-    
-    
-    weatherData.setMeasurements(80, 65, 30.4)
-    print()
-    weatherData.setMeasurements(82, 70, 29.2)
-    print()
-    weatherData.setMeasurements(78, 90, 29.2)
+    weatherData = WeatherData()  # Subject
+    currentDisplay = CurrentConditionsDisplay()  # Observer
+    statisticsDisplay = StatisticsDisplay()  # Observer
+    forecastDisplay = ForecastDisplay()  # Observer
+    heatIndexDisplay = HeatIndexDisplay()  # Observer
+    currentDisplay.observe(weatherData)  # Observation　of weatherData Start
+    statisticsDisplay.observe(weatherData)  # Observation　of weatherData Start
+    forecastDisplay.observe(weatherData)  # Observation　of weatherData Start
+    heatIndexDisplay.observe(weatherData)  # Observation　of weatherData Start
+    weatherData.setMeasurements(80, 65, 30.4)  # Subjectを変化させる
+    print()  # 空行の出力。
+    weatherData.setMeasurements(82, 70, 29.2)  # Subjectを変化させる
+    print()  # 空行の出力。
+    weatherData.setMeasurements(78, 90, 29.2)  # Subjectを変化させる
     
